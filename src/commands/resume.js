038e65fd115f014f23ae917f50950eb9143bdaad
Marcin Krysiński <marcin_krysinski@outlook.com>
2022-01-07 22:17:46 +0100

    added resume command

--- src/commands/resume.js
+++ src/commands/resume.js
@@ -1,18 +1,18 @@
 module.exports = {
   data: {
-    name: 'pause',
-    description: 'pauses the song'
+    name: 'resume',
+    description: 'resumes paused song',
   },
   async execute({ interaction, player }) {
     const queue = player.getQueue(interaction.guild.id);
     if (!queue) {
       return await interaction.channel.send('No music currently playing');
     }
-
-    const isSuccess = queue.setPaused(true);
+    const isSuccess = queue.setPaused(false);
     const msg = isSuccess
-      ? `${queue.current.title} has been paused`
+      ? `${queue.current.title} has been resumed`
       : 'Something went wrong';
+
     return await interaction.channel.send(msg);
   }
 }
